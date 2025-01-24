8cc29a2c0776faa28e0d110b4b07141e94ad0044
Revathiasina <revathiasina13@gmail.com>
2025-01-24 16:22:38 +0530

    set up for task management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './admin/users/users.component';
+import { TasksComponent } from './admin/tasks/tasks.component';
+import { ViewProfileComponent } from './user/view-profile/view-profile.component';
+import { AllTasksComponent } from './user/all-tasks/all-tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'admin/users',
+    component: UsersComponent,
+  },
+  {
+    path: 'user/tasks',
+    component: TasksComponent,
+  },
+  {
+    path: 'user/view-profile',
+    component: ViewProfileComponent,
+  },
+  {
+    path: 'admin/all-tasks',
+    component: AllTasksComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ViewProfileComponent } from './user/view-profile/view-profile.component';
+import { AllTasksComponent } from './user/all-tasks/all-tasks.component';
+import { TasksComponent } from './admin/tasks/tasks.component';
+import { UsersComponent } from './admin/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UsersComponent,
+    TasksComponent,
+    ViewProfileComponent,
+    AllTasksComponent
   ],
   imports: [
     BrowserModule,
